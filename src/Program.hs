182c7d8ddfd068722fa105c400f3e47110f6b20b
Ben Butler-Cole <ben@bridesmere.com>
2025-11-02 15:53:10 +0000

    Move HTML formating to its own module

--- src/Program.hs
+++ src/Program.hs
@@ -19,60 +19,8 @@ import Effects.Curl qualified as Curl
 import Effects.WriteFile (WriteFile, writeFile)
 import Effects.WriteFile qualified as WriteFile
 
+import Display qualified
 import Forecast qualified
-
-formatHtml :: Forecast.AreaForecast -> String
-formatHtml forecast =
-    "<!DOCTYPE html>\n"
-        ++ "<html>\n"
-        ++ "<head>\n"
-        ++ "<meta charset=\"UTF-8\">\n"
-        ++ "<title>Weather</title>\n"
-        ++ "</head>\n"
-        ++ "<body>\n"
-        ++ "<h1>"
-        ++ Forecast.areaName forecast
-        ++ "</h1>\n"
-        ++ "<h2>24 Hour Forecast</h2>\n"
-        ++ "<dl>\n"
-        ++ "<dt>Wind</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.wind (Forecast.current24Hours forecast)
-        ++ "</dd>\n"
-        ++ "<dt>Sea State</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.sea (Forecast.current24Hours forecast)
-        ++ "</dd>\n"
-        ++ "<dt>Weather</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.weather (Forecast.current24Hours forecast)
-        ++ "</dd>\n"
-        ++ "<dt>Visibility</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.visibility (Forecast.current24Hours forecast)
-        ++ "</dd>\n"
-        ++ "</dl>\n"
-        ++ "<h2>Next 24 Hours</h2>\n"
-        ++ "<dl>\n"
-        ++ "<dt>Wind</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.wind (Forecast.next24Hours forecast)
-        ++ "</dd>\n"
-        ++ "<dt>Sea State</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.sea (Forecast.next24Hours forecast)
-        ++ "</dd>\n"
-        ++ "<dt>Weather</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.weather (Forecast.next24Hours forecast)
-        ++ "</dd>\n"
-        ++ "<dt>Visibility</dt>\n"
-        ++ "<dd>"
-        ++ Forecast.visibility (Forecast.next24Hours forecast)
-        ++ "</dd>\n"
-        ++ "</dl>\n"
-        ++ "</body>\n"
-        ++ "</html>\n"
 
 program :: (Member Trace r, Member WriteFile r, Member Curl r, Member (Error String) r) => Sem r ()
 program = do
@@ -82,7 +30,7 @@ program = do
     let forecast = Forecast.parse page
     forecast' <- Error.note "couldn't parse forecast" forecast
     trace "Parsed forecast"
-    writeFile "output/index.html" (formatHtml forecast')
+    writeFile "output/index.html" (Display.formatHtml forecast')
     trace "Finished"
 
 runAll :: Sem [Trace, WriteFile, Curl, Error String, Embed IO] () -> IO ()
