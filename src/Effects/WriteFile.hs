8c96376edc4fc69a4567c49e07e7ee17664ae73e
Ben Butler-Cole <ben@bridesmere.com>
2025-11-02 12:16:46 +0000

    Replace single branch cases with lambdas

--- src/Effects/WriteFile.hs
+++ src/Effects/WriteFile.hs
@@ -19,12 +19,11 @@ writeFile :: (Member WriteFile r) => FilePath -> String -> Sem r ()
 writeFile path content = Polysemy.send (WriteFile path content :: WriteFile (Sem r) ())
 
 runToIO :: (Member (Embed IO) r) => Sem (WriteFile ': r) a -> Sem r a
-runToIO = interpret \case
-    WriteFile path content -> embed do
-        createDirectoryIfMissing True (takeDirectory path)
-        Prelude.writeFile path content
+runToIO = interpret \(WriteFile path content) -> embed do
+    createDirectoryIfMissing True (takeDirectory path)
+    Prelude.writeFile path content
 
 runToList :: Sem (WriteFile ': r) a -> Sem r ([String], a)
 runToList =
-    Writer.runWriter . reinterpret \case
-        WriteFile path content -> Writer.tell [path ++ ": " ++ content]
+    Writer.runWriter . reinterpret \(WriteFile path content) ->
+        Writer.tell [path ++ ": " ++ content]
