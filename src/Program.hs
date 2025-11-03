43bac2638daf50cc9549b2eeedf3c821b514517f
Ben Butler-Cole <ben@bridesmere.com>
2025-11-03 20:54:47 +0000

    Remove tracing which doesn't really give us anything

--- src/Program.hs
+++ src/Program.hs
@@ -11,8 +11,6 @@ import Polysemy.Error.Extended (Error)
 import Polysemy.Error.Extended qualified as Error
 import Polysemy.Reader.Extended (Reader)
 import Polysemy.Reader.Extended qualified as Reader
-import Polysemy.Trace.Extended (Trace, trace)
-import Polysemy.Trace.Extended qualified as Trace
 
 import Effects.Curl (Curl, curl)
 import Effects.Curl qualified as Curl
@@ -22,24 +20,19 @@ import Effects.WriteFile qualified as WriteFile
 import Display qualified
 import Forecast qualified
 
-program :: (Member Trace r, Member WriteFile r, Member Curl r, Member (Error String) r) => Sem r ()
+program :: (Member WriteFile r, Member Curl r, Member (Error String) r) => Sem r ()
 program = do
-    trace "Starting"
     page <- curl Forecast.inshoreWatersUrl
-    trace "Got forecast"
     forecast <- Error.note "couldn't parse forecast" $ Forecast.parse page
-    trace "Parsed forecast"
     output <- Error.fromEither $ Display.formatHtml forecast
     writeFile "output/index.html" output
-    trace "Finished"
 
-runAll :: Sem [Trace, WriteFile, Curl, Error String, Embed IO] () -> IO ()
-runAll = Trace.runToIO >>> WriteFile.runToIO >>> Curl.runToIOError >>> Error.runToIO >>> runM
+runAll :: Sem [WriteFile, Curl, Error String, Embed IO] () -> IO ()
+runAll = WriteFile.runToIO >>> Curl.runToIOError >>> Error.runToIO >>> runM
 
-runPure :: Map String String -> Sem [Trace, WriteFile, Curl, Reader (Map String String), Error String] () -> Either String [(FilePath, String)]
+runPure :: Map String String -> Sem [WriteFile, Curl, Reader (Map String String), Error String] () -> Either String [(FilePath, String)]
 runPure websites =
-    Trace.ignore
-        >>> WriteFile.runToList
+    WriteFile.runToList
         >>> Curl.runToReaderError
         >>> Reader.run websites
         >>> Error.runToEither
