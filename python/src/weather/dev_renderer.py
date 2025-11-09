16090f4eac3517d8eee28ee9d838d64fbb12fb68
Ben Butler-Cole <ben@bridesmere.com>
2025-11-09 09:33:22 +0000

    Move Python implementation to a subdirectory

--- python/src/weather/dev_renderer.py
+++ python/src/weather/dev_renderer.py
@@ -6,7 +6,7 @@ from .renderer import render_html
 def main() -> None:
     """Render HTML for local development."""
     template_path = "src/weather/templates/index.html"
-    output_path = "output/index.html"
+    output_path = "../output/index.html"
     render_html(template_path, output_path)
 
 
--- python/src/weather/renderer.py
+++ python/src/weather/renderer.py
@@ -547,6 +547,6 @@ def render_html(
 if __name__ == "__main__":
     # Default paths
     template_path = "src/weather/templates/index.html"
-    output_path = "docs/index.html"
+    output_path = "../docs/index.html"
 
     render_html(template_path, output_path)
